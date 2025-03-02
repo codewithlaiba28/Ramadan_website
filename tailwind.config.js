71ffa0cbaf9c33c70cb74ae053626aec85200729
laiba khan <codewithlaiba28@gmail.com>
2025-03-02 15:52:17 +0500

    add

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Ensure this is correct
+  theme: {
+    extend: {},
+    fontFamily: {
+      italianno: ["Italianno", "cursive"],
+    },
+  },
+  plugins: [],
+};
+
+
